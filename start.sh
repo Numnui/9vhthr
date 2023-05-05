fec78846901e11508e29324749006e254203c0ef
Numnui <129123403+Numnui@users.noreply.github.com>
2023-05-05 08:51:17 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=1bd8b604-b469-4b3b-bbaa-b3d876c09052
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.bing.com
+
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
